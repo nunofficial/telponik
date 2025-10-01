bce4a2f3a8405b36be6586605b15c3867639593a
usermaxx <aaa.suli@mail.ru>
2025-10-01 08:33:03 +0500

    adaptive

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const images = document.querySelectorAll('.slides img');
 let index = 0;
 
 function showSlide(i) {
+  const slideWidth = images[0].clientWidth;
+  
   if (i < 0) index = images.length - 1;
   else if (i >= images.length) index = 0;
   else index = i;
 
-  slides.style.transform = `translateX(${-index * 500}px)`; // 250 = ширина контейнера
+  slides.style.transform = `translateX(${-index * slideWidth}px)`;
 }
 
 document.querySelector('.prev').onclick = () => showSlide(index - 1);
 document.querySelector('.next').onclick = () => showSlide(index + 1);
+
+
+showSlide(0)
